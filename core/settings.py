dd52bd943dd371c1f19a6cb2f27d579d82fd841d
Stefania Giannina Arias Farias <stefaniaarias@Stefanias-MacBook-Pro.local>
2023-03-20 19:29:10 -0300

    refactor

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET-KEY')
+SECRET_KEY = 'django-insecure-ed@)1efddt)&2tl(^rp^d@p!y4@ipn1xac%=7x&&fpp7od59_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
